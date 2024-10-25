c90cb0e58f34e33a97efce70f5e59c6abd4e1649
AashishBasnet <basnetashish756@gmail.com>
2024-10-25 13:27:49 +0545

    Edit Profile

--- Home/urls.py
+++ Home/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.HomeView, name= 'home'),
+    path("", views.HomeView, name='home'),
     path("about/", views.AboutView, name='about'),
     path("login/", views.UserLoginView, name='login'),
     path("logout/", views.UserLogoutView, name='logout'),
@@ -10,5 +10,6 @@ urlpatterns = [
     path("update-password/", views.UpdatePasswordView, name='update-password'),
     path("product/<slug:slug>", views.SingleProductView, name='product'),
     path("category/<slug>", views.ProductCategoryView, name='category'),
-    path("update-user/", views.UpdateUserView, name = 'update-user')
+    path("update-user/", views.UpdateUserView, name='update-user'),
+    path("update-info/", views.UpdateUserInfoView, name='update-info'),
 ]
--- Home/views.py
+++ Home/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render, redirect
 from django.http import Http404
-from .models import Product, Categories
+from .models import Product, Categories, Profile
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .forms import SignUpForm, UpdateUserForm, ChangePasswordForm
+from .forms import SignUpForm, UpdateUserForm, ChangePasswordForm, UserInfoForm
 from django import forms
 # Create your views here.
-
 
 
 def HomeView(request):
@@ -57,8 +56,9 @@ def UserRegisterView(request):
             password = form.cleaned_data['password1']
             user = authenticate(username=username, password=password)
             login(request, user)
-            messages.success(request, ("You have registered successfully"))
-            return redirect('home')
+            messages.success(
+                request, ("User Created successfully, Please Fill Out Your Info Below"))
+            return redirect('update-info')
         else:
             messages.success(
                 request, ("OOPS! There Was a Problem Logging You in. Please Try Again!"))
@@ -80,62 +80,77 @@ def SingleProductView(request, slug):
 def ProductCategoryView(request, slug):
 
     try:
-        product_categories = Categories.objects.get(slug = slug)
-        products = Product.objects.filter(product_category = product_categories)
+        product_categories = Categories.objects.get(slug=slug)
+        products = Product.objects.filter(product_category=product_categories)
         categories = Categories.objects.all()
         return render(request, 'Home/product_category_template.html',
                       {
                           'products': products,
                           'product_categories': product_categories,
-                          'categories' : categories
+                          'categories': categories
                       })
-
 
     except:
         messages.success(request, ("The Category Doesn't exist"))
         return redirect('home')
 
+
 def UpdateUserView(request):
     if request.user.is_authenticated:
-        current_user = User.objects.get(id = request.user.id)
-        user_form = UpdateUserForm(request.POST or None, instance = current_user)
+        current_user = User.objects.get(id=request.user.id)
+        user_form = UpdateUserForm(request.POST or None, instance=current_user)
         if user_form.is_valid():
             user_form.save()
 
             login(request, current_user)
-            messages.success(request,"Your Profile has been updated")
+            messages.success(request, "Your Profile has been updated")
             return redirect('home')
-        return render(request, "Home/update_user_template.html",{
-        'user_form': user_form })
-    
+        return render(request, "Home/update_user_template.html", {
+            'user_form': user_form})
+
     else:
-        messages.success(request,"You must be logged in to access the page")
+        messages.success(request, "You must be logged in to access the page")
         return redirect('home')
 
-    return render(request, "Home/update_user_template.html",{})
+    return render(request, "Home/update_user_template.html", {})
+
 
 def UpdatePasswordView(request):
     if request.user.is_authenticated:
         current_user = request.user
-        #did they fill out the form
+        # did they fill out the form
         if request.method == 'POST':
-            form = ChangePasswordForm(current_user,request.POST)
-            #is the form valid
+            form = ChangePasswordForm(current_user, request.POST)
+            # is the form valid
             if form.is_valid():
                 form.save()
-                messages.success(request,"Your Password has been updated ")
+                messages.success(request, "Your Password has been updated ")
                 login(request, current_user)
                 return redirect('home')
             else:
                 for error in list(form.errors.values()):
-                    messages.error(request,error)
+                    messages.error(request, error)
                     return redirect('update-password')
 
         else:
             form = ChangePasswordForm(current_user)
-            return render(request, "Home/update_password_template.html",{"form":form})
+            return render(request, "Home/update_password_template.html", {"form": form})
     else:
-        messages.success(request,"You must be logged in to access the page")
+        messages.success(request, "You must be logged in to access the page")
         return redirect('home')
-        
 
+
+def UpdateUserInfoView(request):
+    if request.user.is_authenticated:
+        current_user = Profile.objects.get(user__id=request.user.id)
+        form = UserInfoForm(request.POST or None, instance=current_user)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Your Info has been updated")
+            return redirect('home')
+        return render(request, "Home/update_user_info_template.html", {
+            'form': form})
+
+    else:
+        messages.success(request, "You must be logged in to access the page")
+        return redirect('home')
