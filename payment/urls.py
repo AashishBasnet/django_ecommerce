07fda75aef95550a533e19fdb6585ca1be59b2f1
AashishBasnet <basnetashish756@gmail.com>
2024-10-28 12:34:12 +0545

    generated orders

--- payment/urls.py
+++ payment/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path("payment-success", views.PaymentSuccessView, name='payment-success'),
     path("checkout", views.CheckoutView, name='checkout'),
     path("billing-info", views.BillingInfoView, name='billing-info'),
+    path("process-order", views.ProcessOrderView, name='process-order'),
+
 
 ]
--- payment/views.py
+++ payment/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from cart.cart import Cart
 from .forms import ShippingForm, PaymentForm
-from .models import ShippingAddress
+from .models import ShippingAddress, Order, OrderItem
+from django.contrib.auth.models import User
 from django.contrib import messages
 # Create your views here.
 
@@ -12,6 +13,10 @@ def BillingInfoView(request):
         cart_products = cart.get_prods
         quantities = cart.get_quants
         totals = cart.cart_total()
+
+        # create a session with shipping info
+        my_shipping = request.POST
+        request.session['my_shipping'] = my_shipping
 
         # check to see if user is logged in
         if request.user.is_authenticated:
@@ -84,3 +89,48 @@ def CheckoutView(request):
         })
 
     return render(request, "payment/checkout_template.html", {})
+
+
+def ProcessOrderView(request):
+    if request.POST:
+        # get the cart
+        cart = Cart(request)
+        cart_products = cart.get_prods
+        quantities = cart.get_quants
+        totals = cart.cart_total()
+        payment_form = PaymentForm(request.POST or None)
+        # get shipping session data
+        my_shipping = request.session.get('my_shipping')
+
+        # Gather order info
+        full_name = my_shipping['shipping_full_name']
+        email = my_shipping['shipping_email']
+
+        # create shipping address from session info
+        shipping_address = f"{my_shipping['shipping_address1']}\n{my_shipping['shipping_address2']}\n{my_shipping['shipping_city']}\n{
+            my_shipping['shipping_state']}\n{my_shipping['shipping_zipcode']}\n{my_shipping['shipping_country']}"
+        amount_paid = totals
+
+        if request.user.is_authenticated:
+            # logged in
+            user = request.user
+            # create Order
+            create_order = Order(user=user, full_name=full_name, email=email,
+                                 shipping_address=shipping_address, amount_paid=amount_paid)
+            create_order.save()
+
+            messages.success(request, "Order Placed!")
+            return redirect('home')
+
+        else:
+            # not logged in
+            # create Order
+            create_order = Order(full_name=full_name, email=email,
+                                 shipping_address=shipping_address, amount_paid=amount_paid)
+            create_order.save()
+
+            messages.success(request, "Order Placed!")
+            return redirect('home')
+    else:
+        messages.success(request, "Access Denied")
+        return redirect('home')
