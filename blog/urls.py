fc9accf19fa24b21e818dd5e6b31e4e43a1fce69
AashishBasnet <basnetashish756@gmail.com>
2024-12-02 16:38:30 +0545

    added category and tags

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path("", views.AllBlogsView, name='blog'),
+    path("blog-category/<slug:slug>",
+         views.BlogCategoryView, name='blog-category'),
+    path("blog-tag/<slug:slug>", views.BlogTagView, name='blog-tag'),
     path("<slug:slug>", views.SingleBlogView, name='single-blog'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,10 @@ from django.core.paginator import Paginator, PageNotAnInteger
 
 def AllBlogsView(request):
     posts = Post.objects.all().order_by('-id')
+    latest_posts = Post.objects.all().order_by('-id')[:3]
     paginator = Paginator(posts, 9)
+    tags = Tag.objects.all()
+    all_categories = Category.objects.all()
 
     page = request.GET.get('page')
     try:
@@ -15,7 +18,7 @@ def AllBlogsView(request):
     except EmptyPage:
         posts_paginated = paginator.page(paginator.num_pages)
 
-    return render(request, "blog/all_blogs_template.html", {'posts': posts_paginated})
+    return render(request, "blog/all_blogs_template.html", {'posts': posts_paginated, 'all_categories': all_categories, 'tags': tags, 'latest_posts': latest_posts})
 
 
 def SingleBlogView(request, slug):
@@ -29,3 +32,53 @@ def SingleBlogView(request, slug):
                    "previous_post": previous_post,
                    "next_post": next_post,
                    })
+
+
+def BlogCategoryView(request, slug):
+    try:
+        category = get_object_or_404(Category, slug=slug)
+        all_categories = Category.objects.all()
+        posts = Post.objects.filter(category=category).order_by('-id')
+
+        paginator = Paginator(posts, 9)
+        page_number = request.GET.get('page')
+        paginated_posts = paginator.get_page(page_number)
+
+        tags = Tag.objects.all()
+        latest_posts = Post.objects.all().order_by('-id')[:3]
+
+        return render(request, 'blog/category_template.html', {
+            'category': category,
+            'posts': paginated_posts,
+            'tags': tags,
+            'latest_posts': latest_posts,
+            'all_categories': all_categories
+        })
+    except Category.DoesNotExist:
+        messages.warning(request, "The selected category does not exist.")
+        return redirect('blog')
+
+
+def BlogTagView(request, slug):
+    try:
+        tag = get_object_or_404(Tag, slug=slug)
+        all_categories = Category.objects.all()
+        posts = Post.objects.filter(tags=tag).order_by('-id')
+
+        paginator = Paginator(posts, 9)
+        page_number = request.GET.get('page')
+        paginated_posts = paginator.get_page(page_number)
+
+        tags = Tag.objects.all()
+        latest_posts = Post.objects.all().order_by('-id')[:3]
+
+        return render(request, 'blog/tag_template.html', {
+            'tag': tag,
+            'posts': paginated_posts,
+            'tags': tags,
+            'latest_posts': latest_posts,
+            'all_categories': all_categories
+        })
+    except Tag.DoesNotExist:
+        messages.warning(request, "The selected tag does not exist.")
+        return redirect('blog')
